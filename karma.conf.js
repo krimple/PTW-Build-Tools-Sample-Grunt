b6fce2ab15a22eb0b618ec7a6902abf3aee9b202
Ken Rimple <krimple@chariotsolutions.com>
2014-04-10 12:01:16 -0400

    update

--- karma.conf.js
+++ karma.conf.js
@@ -7,6 +7,7 @@ module.exports = function(config) {
     files: [
       'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
+      'bower_components/angular-mocks/angular-mocks.js',
       'app/scripts/**/*.js',
       'test/spec/**/*.js'
     ],
